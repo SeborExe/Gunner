fcc0c22366bf93224c3fd1d56abd108da0028f7b
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-07-31 16:50:22 +0200

    GameManager
    GameManager has been created. Also room template has been created.

---
+++ Gunner/Assets/__Scripts/GameManager/GameManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+    [Header("Dungeon Levels")]
+
+    [SerializeField] private List<DungeonLevelSO> dungeonLevelList;
+    [SerializeField] private int currentDungeonLevelListIndex = 0;
+    [HideInInspector] public GameState gameState;
+
+    private void Start()
+    {
+        gameState = GameState.gameStarted;
+    }
+
+    private void Update()
+    {
+        HandleGameState();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameState = GameState.gameStarted;
+        }
+    }
+
+    private void HandleGameState()
+    {
+        switch (gameState)
+        {
+            case GameState.gameStarted:
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+                gameState = GameState.playingLevel;
+                break;
+        }
+    }
+
+    private void PlayDungeonLevel(int dungeonLevelListIndex)
+    {
+        
+    }
+
+    #region Validation
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(dungeonLevelList), dungeonLevelList);
+    }
+
+#endif
+    #endregion
+}
