e07874b3cd9767c60fee5236b91ec198e463cc4f
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-08-09 20:27:56 +0200

    Cursor follow
    Now camera follow player and cursor.

--- Gunner/Assets/__Scripts/Misc/CinemachineTarget.cs
+++ Gunner/Assets/__Scripts/Misc/CinemachineTarget.cs
@@ -9,6 +9,8 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    [SerializeField] private Transform cursorTarget;
+
     private void Awake()
     {
         cinemachineTargetGroup = GetComponent<CinemachineTargetGroup>();
@@ -19,17 +21,33 @@ public class CinemachineTarget : MonoBehaviour
         SetCinemachineTargetGroup();
     }
 
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
+    }
+
     private void SetCinemachineTargetGroup()
     {
         CinemachineTargetGroup.Target cinemachineTargetGroup_player = new CinemachineTargetGroup.Target
         {
             weight = 1f,
-            radius = 1f,
+            radius = 2.5f,
             target =
             GameManager.Instance.GetPlayer().transform
         };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineTargetGroup_player };
+        CinemachineTargetGroup.Target cinemachineTargetGroup_cursor = new CinemachineTargetGroup.Target 
+        {
+            weight = 1f, 
+            radius = 1f, 
+            target = cursorTarget 
+        };
+
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] 
+        { 
+            cinemachineTargetGroup_player,
+            cinemachineTargetGroup_cursor
+        };
 
         cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
     }
