dbff1c356a6557118c3ec5c2bcfd4890dd7f8245
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-08-09 12:22:20 +0200

    Aiming
    Weapon aiming system has been created.

--- Gunner/Assets/__Scripts/Player/Player.cs
+++ Gunner/Assets/__Scripts/Player/Player.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+
 #region Require components
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
@@ -12,18 +13,29 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(PolygonCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Health))]
+[RequireComponent(typeof(IdleEvent))]
+[RequireComponent(typeof(Idle))]
+[RequireComponent(typeof(AimWeaponEvent))]
+[RequireComponent(typeof(AimWeapon))]
+[RequireComponent(typeof(PlayerControl))]
+[RequireComponent(typeof(AnimatePlayer))]
 [DisallowMultipleComponent]
 #endregion
 public class Player : MonoBehaviour
 {
     [HideInInspector] public PlayerDetailsSO playerDetails;
     [HideInInspector] public Health health;
+    [HideInInspector] public IdleEvent idleEvent;
+    [HideInInspector] public AimWeaponEvent aimWeaponEvent;
     [HideInInspector] public SpriteRenderer spriteRenderer;
     [HideInInspector] public Animator animator;
 
     private void Awake()
     {
         health = GetComponent<Health>();
+        idleEvent = GetComponent<IdleEvent>();
+        aimWeaponEvent = GetComponent<AimWeaponEvent>();
+
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
---
+++ Gunner/Assets/__Scripts/Player/PlayerControl.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private Transform weaponShootPosition;
+
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        MovementInput();
+        WeaponInput();
+    }
+
+    private void MovementInput()
+    {
+        player.idleEvent.CallIdleEvent();
+    }
+
+    private void WeaponInput()
+    {
+        Vector3 weaponDirection;
+        float weaponAngleDegrees, playerAngleDegrees;
+        AimDirection playerAimDirection;
+
+        AimWeaponInput(out weaponDirection, out weaponAngleDegrees, out playerAngleDegrees, out playerAimDirection);
+    }
+
+    private void AimWeaponInput(out Vector3 weaponDirection, out float weaponAngleDegrees, out float playerAngleDegrees, out AimDirection playerAimDirection)
+    {
+        Vector3 mouseWorldPosition = HelperUtilities.GetMouseWorldPosition();
+
+        weaponDirection = (mouseWorldPosition - weaponShootPosition.position);
+
+        Vector3 playerDirection = (mouseWorldPosition - transform.position);
+
+        //Get weapon to cursor angle
+        weaponAngleDegrees = HelperUtilities.GetAngleFromVector(weaponDirection);
+
+        //Get player to cursor angle
+        playerAngleDegrees = HelperUtilities.GetAngleFromVector(playerDirection);
+
+        playerAimDirection = HelperUtilities.GetAimDirection(playerAngleDegrees);
+
+        player.aimWeaponEvent.CallAimWeaponEvent(playerAimDirection, playerAngleDegrees, weaponAngleDegrees, weaponDirection);
+    }
+}
