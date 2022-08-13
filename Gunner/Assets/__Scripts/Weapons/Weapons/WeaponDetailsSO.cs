69fdf9f3c02283c53c671a2586bc036927ffa087
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-08-13 13:35:03 +0200

    WeaponSO and Pistol
    WeaponSO script and pistol details SO has been created.

---
+++ Gunner/Assets/__Scripts/Weapons/Weapons/WeaponDetailsSO.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponDetails_", menuName = "Scriptable Objects/Weapons/Weapon Details")]
+public class WeaponDetailsSO : ScriptableObject
+{
+    [Header("Weapon Base Details")]
+    public string weaponName;
+    public Sprite weaponSprite;
+
+    [Header("Weapon configuration")]
+    public Vector3 weaponShootPosition;
+    //public AmmoDetailsSO weaponCurrentAmmo;
+
+    [Header("Weapon operating values")]
+    public bool hasInfiniteAmmo = false;
+    public bool hasInfiniteClipCapacity = false;
+    public int weaponClipAmmoCapacity = 6;
+    public int weaponAmmoCapacity = 100;
+    public float weaponFireRate = 0.2f;
+    public float weaponPrechargeTime = 0f;
+    public float weaponReloadTime = 0f;
+
+    #region Validation
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(weaponName), weaponName);
+        //HelperUtilities.ValidateCheckNullValue(this, nameof(weaponCurrentAmmo), weaponCurrentAmmo);
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponFireRate), weaponFireRate, false);
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponPrechargeTime), weaponPrechargeTime, false);
+
+        if (!hasInfiniteAmmo)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponAmmoCapacity), weaponAmmoCapacity, false);
+        }
+
+        if (!hasInfiniteClipCapacity)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(weaponClipAmmoCapacity), weaponClipAmmoCapacity, false);
+        }
+    }
+
+#endif
+    #endregion
+}
