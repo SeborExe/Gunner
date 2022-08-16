f273c4415b7801a006184bd3856df122e94519cd
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-08-16 08:40:18 +0200

    Weapon UI
    Weapon UI has been finished.

--- Gunner/Assets/__Scripts/UI/WeaponStatusUI.cs
+++ Gunner/Assets/__Scripts/UI/WeaponStatusUI.cs
@@ -59,7 +59,7 @@ public class WeaponStatusUI : MonoBehaviour
 
     private void ReloadWeaponEvent_OnReloadWeapon(ReloadWeaponEvent reloadWeaponEvent, ReloadWeaponEventArgs reloadWeaponEventArgs)
     {
-        UpdateReloadWeaponBar(reloadWeaponEventArgs.weapon);
+        UpdateWeaponReloadBar(reloadWeaponEventArgs.weapon);
     }
 
     private void WeaponReloadedEvent_OnWeaponReloaded(WeaponReloadedEvent weaponReloadedEvent, WeaponReloadedEventArgs weaponReloadedEventArgs)
@@ -135,6 +135,97 @@ public class WeaponStatusUI : MonoBehaviour
             GameObject ammoIcon = Instantiate(GameResources.Instance.ammoIconPrefab, ammoHolderTransform);
             ammoIcon.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, Settings.uiAmmoIconSpacing * i);
             ammoIconList.Add(ammoIcon);
+        }
+    }
+
+    private void ClearAmmoLoadeIcons()
+    {
+        foreach (GameObject ammoIcon in ammoIconList)
+        {
+            Destroy(ammoIcon);
+        }
+
+        ammoIconList.Clear();
+    }
+
+    private void UpdateWeaponReloadBar(Weapon weapon)
+    {
+        if (weapon.weaponDetails.hasInfiniteClipCapacity)
+        {
+            return;
+        }
+
+        StopReloadWeaponCoroutine();
+        UpdateReloadText(weapon);
+
+        reloadWeaponCoroutine = StartCoroutine(UpdateWeaponReloadBarRoutine(weapon));
+    }
+
+    private IEnumerator UpdateWeaponReloadBarRoutine(Weapon currentWeapon)
+    {
+        barImage.color = Color.red;
+
+        while (currentWeapon.isWeaponReloading)
+        {
+            float barFill = currentWeapon.weaponReloadTimer / currentWeapon.weaponDetails.weaponReloadTime;
+            reloadBar.transform.localScale = new Vector3(barFill, 1f, 1f);
+
+            yield return null;
+        }
+    }
+
+    private void ResetWeaponReloadBar()
+    {
+        StopReloadWeaponCoroutine();
+
+        barImage.color = Color.green;
+        reloadBar.transform.localScale = Vector3.one;
+    }
+
+    private void StopReloadWeaponCoroutine()
+    {
+        if (reloadWeaponCoroutine != null)
+        {
+            StopCoroutine(reloadWeaponCoroutine);
+        }
+    }
+
+    private void UpdateReloadText(Weapon weapon)
+    {
+        if ((!weapon.weaponDetails.hasInfiniteClipCapacity) && (weapon.weaponClipRemainingAmmo <= 0 || weapon.isWeaponReloading))
+        {
+            barImage.color = Color.red;
+            StopBlinkingReloadTextCoroutine();
+            blinkingReloadTextCoroutine = StartCoroutine(StartBlinkingReloadTextRoutine());
+        }
+        else
+        {
+            StopBlinkingReloadText();
+        }
+    }
+
+    private IEnumerator StartBlinkingReloadTextRoutine()
+    {
+        while (true)
+        {
+            reloadText.text = "RELOAD";
+            yield return new WaitForSeconds(0.3f);
+            reloadText.text = "";
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    private void StopBlinkingReloadText()
+    {
+        StopBlinkingReloadTextCoroutine();
+        reloadText.text = "";
+    }
+
+    private void StopBlinkingReloadTextCoroutine()
+    {
+        if (blinkingReloadTextCoroutine != null)
+        {
+            StopCoroutine(blinkingReloadTextCoroutine);
         }
     }
 }
