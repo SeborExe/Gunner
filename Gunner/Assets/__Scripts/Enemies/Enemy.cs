d0988dae35118e145d3bed00d42a06c161472eec
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-08-24 11:51:02 +0200

    Enemy and boss
    Enemy and boss SO and prefabs has been created.

---
+++ Gunner/Assets/__Scripts/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector] public EnemyDetailsSO enemyDetails;
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+    [HideInInspector] SpriteRenderer[] spriteRendererArray;
+
+    private void Awake()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
+}
---
+++ Gunner/Assets/__Scripts/Enemies/EnemyDetailsSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDetails_", menuName = "Scriptable Objects/Enemy/Enemy Details")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    [Header("Enemy Details")]
+    public string enemyName;
+    public GameObject enemyPrefab;
+}
