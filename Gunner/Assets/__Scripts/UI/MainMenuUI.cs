bb7d4fc150a0ca0680303d803e3fdfc37c3da5a1
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-09-16 09:43:21 +0200

    Instructions menu
    Insctructions menu with informations how to play has been created.

--- Gunner/Assets/__Scripts/UI/MainMenuUI.cs
+++ Gunner/Assets/__Scripts/UI/MainMenuUI.cs
@@ -8,8 +8,11 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] GameObject playButton;
     [SerializeField] GameObject highScoreButton;
     [SerializeField] GameObject returnToMainMenuButton;
+    [SerializeField] GameObject quitButton;
+    [SerializeField] GameObject instructionsButton;
 
     private bool isHighScoresSceneLoaded = false;
+    private bool isInstructionsSceneLoaded = false;
 
     private void Start()
     {
@@ -28,12 +31,25 @@ public class MainMenuUI : MonoBehaviour
     public void LoadHighScores()
     {
         playButton.SetActive(false);
+        instructionsButton.SetActive(false);
         highScoreButton.SetActive(false);
         isHighScoresSceneLoaded = true;
 
         SceneManager.UnloadSceneAsync("CharacterSelectorScene");
         returnToMainMenuButton.SetActive(true);
         SceneManager.LoadScene("HighScoreScene", LoadSceneMode.Additive);
+    }
+
+    public void LoadInstructions()
+    {
+        playButton.SetActive(false);
+        instructionsButton.SetActive(false);
+        highScoreButton.SetActive(false);
+        isInstructionsSceneLoaded = true;
+
+        SceneManager.UnloadSceneAsync("CharacterSelectorScene");
+        returnToMainMenuButton.SetActive(true);
+        SceneManager.LoadScene("InstructionsScene", LoadSceneMode.Additive);
     }
 
     public void LoadCharacterSelector()
@@ -45,10 +61,21 @@ public class MainMenuUI : MonoBehaviour
             SceneManager.UnloadSceneAsync("HighScoreScene");
             isHighScoresSceneLoaded = false;
         }
+        else if (isInstructionsSceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync("InstructionsScene");
+            isInstructionsSceneLoaded = false;
+        }
 
         playButton.SetActive(true);
         highScoreButton.SetActive(true);
+        instructionsButton.SetActive(true);
 
         SceneManager.LoadScene("CharacterSelectorScene", LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
