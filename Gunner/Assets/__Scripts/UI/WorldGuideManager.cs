f3d3ea1128634d613a56d60d0d3248fb0aa7db83
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2023-01-09 10:35:23 +0100

    Buttons
    Option buttons has been added.

--- Gunner/Assets/__Scripts/UI/WorldGuideManager.cs
+++ Gunner/Assets/__Scripts/UI/WorldGuideManager.cs
@@ -14,4 +14,19 @@ public class WorldGuideManager : MonoBehaviour
     [SerializeField] Button itemButtonOptions;
     [SerializeField] Button usableItemButtonOptions;
     [SerializeField] Button holdingItemButtonOptions;
+
+    [Header("Weapons Options")]
+    [SerializeField] GameObject WeaponsOptions;
+    [SerializeField] Button gunsButtonOptions;
+    [SerializeField] Button meleeButtonOptions;
+
+    [Header("Enemies Options")]
+    [SerializeField] GameObject EnemiesOptions;
+    [SerializeField] Button HedusaButtonOptions;
+    [SerializeField] Button SlimeblockButtonOptions;
+    [SerializeField] Button OrcButtonOptions;
+    [SerializeField] Button GrimonkButtonOptions;
+    [SerializeField] Button MudrockButtonOptions;
+    [SerializeField] Button SlizzardButtonOptions;
+    [SerializeField] Button BossButtonOptions;
 }
